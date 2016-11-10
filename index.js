9194bdde2d5ce882fe5140508b018e62e7faa8c4
matheus92 <lima.eca@gmail.com>
2016-11-10 00:07:29 -0200

    url test

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.get(function(request, response) {
   response.send(request.url);
