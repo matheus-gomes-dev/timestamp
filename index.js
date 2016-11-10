89ce83cab4fd345e6a9e634b38151fa111a54153
matheus92 <lima.eca@gmail.com>
2016-11-10 00:01:36 -0200

    url test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get(function(request, response) {
   response.send(request.url);
   
 });
