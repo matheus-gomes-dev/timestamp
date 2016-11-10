8a914fcc5af06ac05b0c0269099ced83f14eb97b
matheus92 <lima.eca@gmail.com>
2016-11-09 23:03:54 -0200

    url test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+const url = require('url');
 
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,7 +11,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send('hello world');
+  response.send(urlObject.href);
 });
 
 app.listen(app.get('port'), function() {
