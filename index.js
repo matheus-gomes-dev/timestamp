9757f0b8a883d5d81dc20bd275046a8ff077f16c
matheus92 <lima.eca@gmail.com>
2016-11-15 18:55:18 -0200

    url test

--- index.js
+++ index.js
@@ -13,33 +13,19 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, response) {	
-  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  //response.send(fullUrl);
-
-/*
-  if (fullUrl.search("January") != -1)
-    response.send("teste1");
-  else
-    response.send("teste2");
-
-*/
-
-  //response.send(req.get);
+  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl; 
   var parameter = (req.originalUrl).substring(1,(req.originalUrl).length)
-  //response.send(parameter);
+  
   if (isNaN(parameter) && (parameter.indexOf("January") >= 0) && (parameter.indexOf(",") >= 0)){
   	console.log("teste");
   	
   	var month = '01';
-  	var year = parameter.substring((parameter.indexOf("?")+1),parameter.length);
+  	var year = parameter.substring((parameter.indexOf(",")+1),parameter.length);
     var day = parameter.substring((parameter.indexOf("y")+1),(parameter.indexOf(",")));
     var parsedDate = year + '/' + month + '/' + day;
-    var teste = moment('2015-07-12 14:59:23', 'YYYY-MM-DD HH:mm:ss').valueOf()/1000;
-    //console.log(parsedDate);
-    console.log(teste);
-    //response.send(JSON.stringify(teste));//moment returns an object
+    var unixTime = moment(parsedDate, 'YYYY-MM-DD').valueOf()/1000;
     var time = {
-    	unix: teste,
+    	unix: unixTime,
     	natural: "January " + day + ', ' + year 
     };
     response.json(time);
